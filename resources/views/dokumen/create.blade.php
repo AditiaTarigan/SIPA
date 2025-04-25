f992f7a0530591ef83747af94898fc858afc8327
johannessitorus <johannessitorus8@gmail.com>
2025-04-25 08:10:53 +0700

    update dokumen & logacitivity

--- resources/views/dokumen/create.blade.php
+++ resources/views/dokumen/create.blade.php
@@ -24,12 +24,26 @@
 
         <div class="mb-3">
             <label for="prodi" class="form-label">Program Studi</label>
-            <input type="text" class="form-control" id="prodi" name="prodi" required>
+            <select class="form-control" id="prodi" name="prodi" required>
+                <option value="">-- Pilih Program Studi --</option>
+                <option value="Teknologi Informasi">Teknologi Informasi</option>
+                <option value="Teknologi Komputer">Teknologi Komputer</option>
+                <option value="Teknologi Rekayasa Perangkat Lunak"> Teknologi Informasi Perangkat Lunak</option>
+            </select>
         </div>
 
         <div class="mb-3">
             <label for="nomor_kelompok" class="form-label">Nomor Kelompok</label>
             <input type="text" class="form-control" id="nomor_kelompok" name="nomor_kelompok" required>
+        </div>
+        <div class="mb-3">
+            <label for="angkatan" class="form-label">Angkatan</label>
+            <select class="form-control" id="angkatan" name="angkatan" required>
+                <option value="">-- Pilih Angkatan --</option>
+                <option value="2024">2024</option>
+                <option value="2024">2023</option>
+                <option value="2024">2022</option>
+            </select>
         </div>
 
         <div class="mb-3">
